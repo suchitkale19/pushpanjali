8a5a7ba9c371ea65007382b0233b8659aa108f93
suchitkale19 <suchitkale19102004@gmail.com>
2026-06-10 18:53:25 +0530

    applyied route controllers

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -1,30 +1,133 @@
-exports.getProducts = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: "The route hasn't set yet",
-  });
+const Product = require("./../model/productModel");
+const qs = require("qs");
+
+class APIFeatures {
+  constructor(queryModel, queryExpress) {
+    queryModel = this.queryModel;
+    queryExpress = this.queryExpress;
+  }
+
+  filter() {}
+}
+
+exports.getProducts = async (req, res) => {
+  try {
+    let queryObj = { ...req.query };
+    queryObj = qs.parse(queryObj);
+
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("title");
+    }
+
+    if (req.query.fields) {
+      fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // 1 10  11 20 (page-1)*limit    1 (2-1)*10
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numProducts = await Product.countDocuments();
+      if (skip > numProducts) {
+        throw new Error("Products not exists");
+      }
+    }
+
+    const products = await query;
+    res.status(200).json({
+      status: "success",
+      results: products.length,
+      data: {
+        products,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
 };
-exports.getSingleProduct = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: "The route hasn't set yet",
-  });
+exports.getSingleProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err,
+    });
+  }
 };
-exports.addProducts = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: "The route hasn't set yet",
-  });
+exports.addProducts = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+
+    res.status(200).json({
+      status: "success",
+      data: newProduct,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err,
+    });
+  }
 };
-exports.updateProducts = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: "The route hasn't set yet",
-  });
+exports.updateProducts = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err,
+    });
+  }
 };
-exports.deleteProducts = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: "The route hasn't set yet",
-  });
+exports.deleteProducts = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    res.status(20).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: err,
+    });
+  }
 };
--- server/model/productModel.js
+++ server/model/productModel.js
@@ -1,18 +1,36 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: {
+  title: {
     type: String,
-    required: [true, "A Product Must Have Name"],
+    required: [true, "A Product Must Have A Title"],
     unique: true,
+  },
+  description: String,
+  category: {
+    type: String,
+    required: [true, "A Product Must Have A Category"],
+  },
+  includes: [String],
+  price: {
+    type: Number,
+    required: [true, "A product must have price"],
+  },
+  image: {
+    type: String,
+    required: [true, "A Product Must Have A Image"],
+  },
+  discountPercentage: {
+    type: Number,
+    default: 5,
   },
   rating: {
     type: Number,
     default: 4.5,
   },
-  price: {
-    type: Number,
-    required: [true, "A product must have price"],
+  shippingInformation: {
+    type: String,
+    default: "Arrives in 2 days",
   },
 });
 
